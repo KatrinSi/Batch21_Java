92594b1c3ed64c5334c4e2132f2fb55c3125842f
KatrinSi <katrinsi@me.com>
2020-11-13 11:10:45 -0500

    MethodsWithoutParameters2.java

---
+++ src/day29_Methods/MethodsWithoutParameters2.java
@@ -0,0 +1,24 @@
+package day29_Methods;
+
+public class MethodsWithoutParameters2 {
+    public static void main(String[] args) {
+
+        printOddNums1to100();
+        System.out.println("Hello world!");
+        printOddNums1to100();
+
+    }
+
+    public static void printOddNums1to100() {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
+
+}
