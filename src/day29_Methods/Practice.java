d47bb72022b9ad1fe8fcd7f651863cf64be028fb
KatrinSi <katrinsi@me.com>
2020-11-13 10:35:53 -0500

    Practice.java

---
+++ src/day29_Methods/Practice.java
@@ -0,0 +1,23 @@
+package day29_Methods;
+
+public class Practice {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <=5; i++){
+            System.out.println("Hello");
+        }
+        System.out.println("KatrinSi");
+        for (int i = 1; i <= 5; i++){
+            System.out.println("Hello");
+        }
+        System.out.println("Cybertek");
+        for (int i = 1; i <=5; i++){
+            System.out.println("Hello");
+        }
+
+
+
+
+    }
+}
